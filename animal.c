26cf3d75730adba8156a5fc641c3973680cd7124
Sneha <sneha_shirodkar@persistent.com>
2015-01-14 14:37:12 +0530

    Added Dog

--- animal.c
+++ animal.c
@@ -4,9 +4,6 @@ void main()
 	printf("\nAnimals\n\n");
 	printf("\nTiger\n");
 	printf("\nKangaroo\n");
-<<<<<<< HEAD
 	printf("\nDog\n");
-=======
 	printf("\nCat\n");
->>>>>>> f5e6bd96ce32bf69294f4d698323f7e97a60db9e
 }
