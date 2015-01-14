4ca2fea81ec2b281348e650e603d99048a3e2802
Sneha <sneha_shirodkar@persistent.com>
2015-01-14 11:04:19 +0530

    First Mege Resolved

--- color.c
+++ color.c
@@ -6,9 +6,7 @@ void main()
 	printf("\n---------\n");
 	printf("\nBeige\n");
         printf("\nGrey\n");
-<<<<<<< HEAD
+
 	printf("\nGreen\n");
-=======
         printf("\nBlue\n");
->>>>>>> 867305b5205e67bb2c662e403177b012a6445605
 }
